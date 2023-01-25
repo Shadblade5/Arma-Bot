f3f42349574e998ba0162ff28c6bd3988eef608f
Nicholas Burley <nick.burley@protonmail.com>
2023-01-24 20:16:18 -0600

    More tweaks

--- bot/database.py
+++ bot/database.py
@@ -1,4 +1,4 @@
-import mysql.connector as MYSQL
+from mysql import connector as sql
 from datetime import datetime
 import pytz
 
@@ -14,67 +14,65 @@ class Database:
         self.db_name = db_name
         self.retries = 0
 
-    async def executeSQL(self, sql, query=True):
+    async def executeSQL(self, sql_query, query=True):
         try:
             print('Connecting to MYSQL...')
-            client = MYSQL.connect(host=self.db_host,
-                                   user=self.db_username,
-                                   password=self.db_password,
-                                   database=self.db_name)
+            client = sql.connect(host=self.db_host,
+                                 user=self.db_username,
+                                 password=self.db_password,
+                                 database=self.db_name)
             print('Connected.')
             mycursor = client.cursor()
-            mycursor.execute(sql)
+            mycursor.execute(sql_query)
             if query:
                 print('Fetching data')
                 return mycursor.fetchall()
             else:
                 print('Commiting')
                 client.commit()
-        except MYSQL.InterfaceError:
+        except sql.InterfaceError:
             self.retries += 1
-            print('Failed to connect,attempt #{}. Retrying...'.format(self.retries))
+            print(f'Failed to connect,attempt #{self.retries}. Retrying...')
             if (self.retries < 60):
-                return await self.executeSQL(sql, query)
+                return await self.executeSQL(sql_query, query)
             else:
                 print('Server unresponsive.')
-        except MYSQL.IntegrityError as e:
+        except sql.IntegrityError as e:
             print(e)
             return 1
-        except MYSQL.errors as e:
+        except sql.errors as e:
             print(e)
         finally:
             print('Closing connection')
             client.close()
 
     async def getUsers(self):
-        sql = 'SELECT * FROM master'
-        return await self.executeSQL(sql)
+        sql_query = 'SELECT * FROM master'
+        return await self.executeSQL(sql_query)
 
     async def getUserInfo(self, DiscordID):
-        sql = 'SELECT * FROM master WHERE DISCORDID = {}'.format(DiscordID)
-        results = await self.executeSQL(sql=sql, query=True)
+        sql_query = f'SELECT * FROM master WHERE DISCORDID = {DiscordID}'
+        results = await self.executeSQL(sql=sql_query, query=True)
         return results[0]
 
     async def adduser(self, DiscordID, DiscordName):
         now = datetime.now()
         dt_string = now.strftime(
             '%Y-%m-%d %H:%M:%S').astimezone(pytz.timezone('US/Eastern'))
-        sql = "INSERT INTO `master` VALUES ('{0}','{1}','0','0','{2}','NULL');".format(DiscordID, DiscordName,
-                                                                                       dt_string)
-        if await self.executeSQL(sql, False) == 1:
+        sql_query = f"INSERT INTO `master` VALUES ('{DiscordID}','{DiscordName}','0','0','{dt_string}','NULL');"
+        if await self.executeSQL(sql_query, False) == 1:
             return False
         else:
             return True
 
     async def getcoc(self):
-        sql = 'SELECT * FROM coc'
-        return await self.executeSQL(sql)
+        sql_query = 'SELECT * FROM coc'
+        return await self.executeSQL(sql_query)
 
     async def setBirthday(self, DiscordID, birthday):
-        sql = "UPDATE `master` SET `Birthday` = '{0}' WHERE DISCORDID = '{1}'".format(
-            birthday, DiscordID)
+        sql_query = f"UPDATE `master` SET `Birthday` = '{birthday}' WHERE DISCORDID = '{DiscordID}'"
         try:
-            await self.executeSQL(sql, False)
+            await self.executeSQL(sql_query, False)
         except Exception:
             return False
         return True
